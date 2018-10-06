3c202f01de98ada52c83ab4e95674025d3354b36
GT <League@AdministorsiMac.attlocal.net>
2018-10-06 11:56:35 -0700

    Make A Robot Move And Make Four Squares

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,7 +8,20 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+	Robot C3PO=new Robot();
+	C3PO.setSpeed(99999999);
+	C3PO.penDown();
+	C3PO.turn(180);
+	C3PO.move(500);
+		C3PO.turn(180);
+		C3PO.move(500);
+		C3PO.turn(90);
+		C3PO.move(500);
+		C3PO.turn(180);
+		C3PO.move(500);
+		C3PO.turn(90);
+		C3PO.move(500);
+		C3PO.turn(180);
 		
 		
 	}
